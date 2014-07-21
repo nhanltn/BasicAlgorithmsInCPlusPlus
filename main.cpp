4f31db8bf4ef7ba1f9bcad7b3bd93bed2c59e407
OpenShift guest <nhanltn@gmail.com>
2014-07-21 08:28:35 -0400

    Commit

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "================================================" << endl;
-    cout << "Welcome back to List Data Structure using C/C++ " << endl;
-    cout << "================================================" <<  endl;
-}
